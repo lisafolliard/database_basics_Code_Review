625acb26957ac4d3ccb256df0152618f372dfa2a
Lisa Folliard <lisaf37@gmail.com>
2015-08-24 20:05:53 -0700

    add a client to a stylist's roster

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -46,15 +46,15 @@ class Stylist
     DB.exec("DELETE FROM clients WHERE stylist_id = #{self.id()}")
   end
 
-  # define_method(:clients) do
-  #   stylist_clients = []
-  #   clients = DB.exec("SELECT * FROM clients WHERE stylist_id = #{self.id()};")
-  #   clients.each() do |client|
-  #     id = client.fetch('id').to_i()
-  #     name = client.fetch('name')
-  #     stylist_id = client.fetch('stylist_id').to_i()
-  #     stylist_clients.push(Client.new({:name => name, :stylist_id => stylist_id, :id => id}))
-  #   end
-  #   stylist_clients
-  # end
+  define_method(:clients) do
+    stylist_clients = []
+    clients = DB.exec("SELECT * FROM clients WHERE stylist_id = #{self.id()};")
+    clients.each() do |client|
+      id = client.fetch('id').to_i()
+      name = client.fetch('name')
+      stylist_id = client.fetch('stylist_id').to_i()
+      stylist_clients.push(Client.new({:name => name, :stylist_id => stylist_id, :id => id}))
+    end
+    stylist_clients
+  end
 end #class method end
