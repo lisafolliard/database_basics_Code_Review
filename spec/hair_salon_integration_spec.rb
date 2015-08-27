04ef83cef156669da0f86bdba92f7586494e88bb
Lisa Folliard <lisaf37@gmail.com>
2015-08-26 20:42:05 -0700

    pass integration spec to add new client to stylist

--- spec/hair_salon_integration_spec.rb
+++ spec/hair_salon_integration_spec.rb
@@ -47,26 +47,26 @@ describe('how to delete a stylist', {:type => :feature}) do
   end
 end
 
-# describe('add new client to a stylist', {:type => :feature}) do
-#   it('allows user to add clients to stylists') do
-#     new_stylist = Stylist.new({:name => 'Dawn LaCovey', :id => nil})
-#     new_stylist.save()
+# describe('view individual client details', {:type => :feature}) do
+#   it('allows user to click on individual client link on stylist page') do
 #     visit('/')
 #     fill_in('name', :with => 'Dawn LaCovey')
 #     click_button("Add Stylist")
 #     click_link('Dawn LaCovey')
-#     click_button('Add New Client')
-#     expect(page).to have_content("Deanna Monaco")
+#     expect(page).to have_content('Dawn LaCovey')
 #   end
 # end
-#
-#     visit('/')
-#     # click_link(new_stylist.firstname())
-#     click_link('Dawn LaCovey')
-#     click_link('Add a New Client')
-#     fill_in('firstname', :with => 'Deanna')
-#     fill_in('lastname', :with => 'Monaco')
-#     click_button('Add New Client')
-#     expect(page).to have_content('Deanna')
-#   end
-# end
+
+describe('add new client to a stylist', {:type => :feature}) do
+  it('allows user to add clients to stylists') do
+    # new_stylist = Stylist.new({:name => 'Dawn LaCovey', :id => nil})
+    # new_stylist.save()
+    visit('/')
+    fill_in('name', :with => 'Dawn LaCovey')
+    click_button("Add Stylist")
+    click_link('Dawn LaCovey')
+    fill_in('name', :with => 'Deanna Monaco')
+    click_button('Add New Client')
+    expect(page).to have_content("Deanna Monaco")
+  end
+end
